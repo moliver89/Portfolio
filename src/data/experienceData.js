c7bbb592cd39f952cedcc22e8df63687cbcdcd61
Mauro Oliver <olivermauro@outlook.com>
2025-05-27 12:58:39 +0200

    Agregado de experienceData.js y un map en Experiencia.jsx. Ademas de algunos estilos.

---
+++ src/data/experienceData.js
@@ -0,0 +1,15 @@
+export const experience = [
+  {
+    job: 'Vendedor',
+    company: 'Send+ - Accesorios celulares',
+    time: 'Oct 2015 - Abr 2021',
+    description:
+      'Atencion al publico y venta a comision de articulos de telefonia movil',
+  },
+  {
+    job: 'Relaciones publicas',
+    company: 'Plur Eventos',
+    time: 'Jun 2021 - Oct 2021',
+    description: 'Vendedor multilenguaje para eventos turisticos',
+  },
+];
--- src/pages/Experiencia.jsx
+++ src/pages/Experiencia.jsx
@@ -1,4 +1,19 @@
+import '../../styles/Experiencia.css';
+import { experience } from '../data/experienceData';
+
 const Experiencia = () => {
-  return <h2>Aca van las experiencias laborales.</h2>;
+  return (
+    <main>
+      <h2>Experiencia laboral</h2>
+      {experience.map((exp, index) => (
+        <div className='jobDescription'>
+          <h3>{exp.job}</h3>
+          <h4>{exp.company}</h4>
+          <h4>{exp.time}</h4>
+          <p>{exp.description}</p>
+        </div>
+      ))}
+    </main>
+  );
 };
 export default Experiencia;
